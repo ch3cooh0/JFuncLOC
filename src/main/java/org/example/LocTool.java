c003af87362aa6996eb692ca61a0de79b80cce23
ch3cooh0 <46026656+ch3cooh0@users.noreply.github.com>
2025-06-17 01:08:20 +0900

    Remove duplicate Function annotation and update LocTool import

--- src/main/java/org/example/LocTool.java
+++ src/main/java/org/example/LocTool.java
@@ -5,6 +5,7 @@ import soot.*;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Edge;
 import soot.options.Options;
+import org.example.annotation.Function;
 
 import java.io.File;
 import java.io.IOException;
